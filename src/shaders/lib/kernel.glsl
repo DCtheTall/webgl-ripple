264cfba7a3c04ce82807c7b105b04a5ab4a88c5a
Dylan Cutler <dylancutler@google.com>
2019-05-29 00:05:02 -0400

    add explainer to kernel.glsl

--- src/shaders/lib/kernel.glsl
+++ src/shaders/lib/kernel.glsl
@@ -1,3 +1,9 @@
+/*
+
+Utility function for applying a convolution with a 3x3 kernel.
+
+*/
+
 float convoluteMatrices(mat3 A, mat3 B) {
   return dot(A[0], B[0]) + dot(A[1], B[1]) + dot(A[2], B[2]);
 }
